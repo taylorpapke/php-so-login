31ac6fc9f2d776a4aede3aa2c3eabf437595d430
taylorpapke <taylorpapke@gmail.com>
2024-10-30 16:41:16 -0700

    adding error reporting

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+?>
+<?php
 require_once 'vendor/autoload.php';
 
 $config = require 'config.php';
